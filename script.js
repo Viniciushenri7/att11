6d5a8186902a30577cfa816a6557eaa1acf9392e
Viniciushenri7 <148489330+Viniciushenri7@users.noreply.github.com>
2023-12-15 11:16:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function calcularoproduto(a,b){
+    return a*b;
+}
+const x = calcularoproduto(5,7);
+const texto = "O resultado é " + x
+alert(texto)
+
+
+function calcularoproduto(s,t){
+    return s*t;
+}
+const y = calcularoproduto(8,2);
+const text = "O resultado é " + y
+alert(text)
---
+++ styli.js
@@ -0,0 +1,6 @@
+function calcular (c,d){
+    return c*d
+}
+const x = calcular(6,1);
+const texto = "O total é " + x
+alert(texto)
